72cb46b01545778dae9fbfb66d386efc026cfdef
Victoria Sales <contatovictoriasales@gmail.com>
2023-06-14 16:05:41 -0300

    Cria o modelo em src/models/User.js

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+const userModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { 
+      type: DataTypes.INTEGER, 
+      autoIncrement: true, 
+      primaryKey: true 
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    tableName: 'users',
+    underscored: true,
+    timestamps: false,
+  }
+  );
+
+  return User;
+};
+
+module.exports = userModel;
