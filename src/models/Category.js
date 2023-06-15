cf3a4fe5ba0b7c6ae12d44de8c54827afbbeca10
Victoria Sales <contatovictoriasales@gmail.com>
2023-06-15 20:31:02 -0300

    Cria o modelo em src/models/Category.js

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+const categoryModel = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { 
+      type: DataTypes.INTEGER, 
+      autoIncrement: true, 
+      primaryKey: true 
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    tableName: 'categories',
+    underscored: true,
+    timestamps: false,
+  }
+  );
+
+  return Category;
+};
+
+module.exports = categoryModel;
